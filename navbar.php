1fac20f922a45813fe06078dc53e435d02a77d24
Shariar Oni <54840985+shariaroni@users.noreply.github.com>
2024-01-13 21:48:02 +0600

    all responsive code of agrodss.com

---
+++ navbar.php
@@ -0,0 +1,37 @@
+<header class="header">
+  <nav class="navbar navbar-expand-lg bg-success-subtle text-emphasis-success sticky-top p-0">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <img src="img/logo.png" alt="AGRODSS" width="auto" height="40" class="d-inline-block align-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto text-center mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="index.php">হোম</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="machineLearningModel.php">মেশিনলার্নিং মডেল</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dataset.php">ডাটাসেট</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="publications.php">পাবলিকেশন্স</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pesticidesRecommendation.php">কিটনাশক সুপারিশ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">সম্পর্কে</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="app.php">অ্যাপ</a>
+            </li>
+          </ul>
+        </div>
+    </div>
+  </nav>
+</header>
